51b8ee96f4932f46f304860c42e15b8ddf53f553
Luiz Guilherme <123317391+luizx1@users.noreply.github.com>
2025-01-30 20:23:53 -0300

    Update classificadorHeroi.js

--- classificadorHeroi.js
+++ classificadorHeroi.js
@@ -7,22 +7,22 @@ let nivel;
 
 // Estrutura de decisão para classificar o nível do herói
 if (experiencia < 1000) {
-  nivel = "Ferro";
+  nivel = "Ferro"
 } else if (experiencia <= 2000) {
-  nivel = "Bronze";
+  nivel = "Bronze"
 } else if (experiencia <= 5000) {
-  nivel = "Prata";
+  nivel = "Prata"
 } else if (experiencia <= 7000) {
-  nivel = "Ouro";
+  nivel = "Ouro"
 } else if (experiencia <= 8000) {
-  nivel = "Platina";
+  nivel = "Platina"
 } else if (experiencia <= 9000) {
-  nivel = "Ascendente";
+  nivel = "Ascendente"
 } else if (experiencia <= 10000) {
-  nivel = "Imortal";
+  nivel = "Imortal"
 } else {
-  nivel = "Radiante";
+  nivel = "Radiante"
 }
 
 // Saída final
-console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);
+console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`)
